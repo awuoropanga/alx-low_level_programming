0895ccd87b80ac88a7f39008560bfdfdde1f080a
awuoropanga <opngabetty98@gmail.com>
2022-09-09 10:34:43 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,30 @@
+#!/bin/bash
+#include<stdio.h>
+
+
+
+/**
+
+ * main -Entry point
+
+ *
+
+ * Description: 'Printing to the stdout using puts function'
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
+
+int main(void)
+
+{
+
+puts("\"Programming is like building a multilingual puzzle");
+
+
+
+return (0);
+
+}
