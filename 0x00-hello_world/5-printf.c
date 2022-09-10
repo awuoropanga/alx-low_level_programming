91db54bd2e1a816c72c074d7192048fdb79ab3dd
awuoropanga <opngabetty98@gmail.com>
2022-09-10 12:40:28 -0700

    print f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+
+/**
+* main - Print a string
+
+* Description: use printf
+
+* Return:always 0 (success)
+
+*/
+
+
+
+int main(void)
+
+
+
+{
+
+printf("with proper grammar, but the outcome is a piece of art,\n");
+
+return (0);
+
+}
