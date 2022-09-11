b2a30a455d828e4f14bb9cf3c652a3aba041cac6
awuoropanga <opngabetty98@gmail.com>
2022-09-11 12:36:45 -0700

    single digit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (suecces)
+ */
+
+int main(void)
+
+{
+
+int a;
+
+for (a = 48; a <= 57; a++)
+
+{
+
+putchar(a);
+
+}
+
+putchar('\n');
+
+return (0);
+
+}
