f9db103d6dbfbd2d969981f4b970dbfd1f99832c
awuoropanga <opngabetty98@gmail.com>
2022-09-13 14:31:39 -0700

    sign

---
+++ 5-sign.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * print_sign - P n 0
+ * @n: negative, zero or positive
+ * Return: 0
+ */
+
+int print_sign(int n)
+
+{
+
+if (n > 0)
+
+{
+
+_putchar('+');
+return (1);
+
+}
+
+else if (n == 0)
+
+{
+
+_putchar('0');
+return (0);
+
+}
+
+else
+
+{
+
+_putchar('-');
+return (-1);
+
+}
+
+}
