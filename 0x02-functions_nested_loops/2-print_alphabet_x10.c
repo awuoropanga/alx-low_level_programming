142d81c688b05ce5c2e3893eaba399bded02eb55
awuoropanga <opngabetty98@gmail.com>
2022-09-13 13:04:37 -0700

    10 times alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - func
+ *
+ */
+
+void print_alphabet(void)
+
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+
+	{
+		_putchar(alphabet);
+		alphabet++;
+	}
+
+	_putchar('\n');
+}
+
