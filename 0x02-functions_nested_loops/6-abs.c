351ac143acb40b88169ccab3a4c55ea9c14ddf0a
awuoropanga <opngabetty98@gmail.com>
2022-09-14 10:16:52 -0700

    absolute

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,15 +1,28 @@
 #include "main.h"
 
+
+
 /**
- *_abs - prints absolute value
- * @n: var
- * Return: 0
- */
+ *
+ *  *_abs - prints absolute value
+ *
+ *   *@n: var
+ *
+ *    *Return: 0
+ *
+ *     */
+
+
+
 int _abs(int n)
 
 {
+
 	if (n < 0)
+
 		n = (-1) * n;
+
 	return (n);
+
 }
 
