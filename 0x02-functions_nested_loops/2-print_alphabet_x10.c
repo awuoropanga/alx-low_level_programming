54e106551c2801cf9a0bce06b9e1eaac4b803f4a
awuoropanga <opngabetty98@gmail.com>
2022-09-14 09:26:59 -0700

    white space

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,15 @@
 #include "main.h"
 
 /**
- *
  *  *print_alphabet_x10 - alphabet 10 times
  *
- *   *
  *
- *   */
+ */
 
 void print_alphabet_x10(void)
-	
+
 {
-
 char alphabet;
-
 int n = 0;
 
 while (n <= 9)
@@ -25,11 +21,8 @@ while (n <= 9)
 	while (alphabet <= 'z')
 
 	{
-	
 		_putchar(alphabet);
-	
 		alphabet++;
-	
 		}
 
 	_putchar('\n');
