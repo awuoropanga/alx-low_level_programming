05a8c710a0a8bc6767bdde576f589f8985aa8c7f
awuoropanga <opngabetty98@gmail.com>
2022-09-15 12:55:12 -0700

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -27,9 +27,9 @@ void print_times_table(int n)
 				{
 					_putchar(' ');
 					_putchar(' ');
-					_putchar((res % 10 + '0');
+					_putchar((res % 10) + '0');
 				}
-				else if (res >= 10 && res < 100)
+				else if (res >= 10 && res < 100);
 				{
 					_putchar(' ');
 					_putchar((res / 10) + '0');
