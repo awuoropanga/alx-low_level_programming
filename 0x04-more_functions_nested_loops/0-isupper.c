7ed2828c6589d39897b9796f683eb17e03ee708b
awuoropanga <opngabetty98@gmail.com>
2022-09-15 14:12:52 -0700

    is upper and main.h

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+
+
+/**
+
+ * _isupper - func name
+
+ *@c: var name
+
+ * Return: 0
+
+ */
+
+
+
+int _isupper(int c)
+
+{
+
+if (c >= 'A' && c <= 'Z')
+
+{
+
+return (1);
+
+}
+
+else
+
+{
+
+return (0);
+
+}
+
+}
