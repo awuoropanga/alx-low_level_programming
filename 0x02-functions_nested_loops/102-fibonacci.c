289e811a83a8024f380a2a28daba32eae1c3fe0f
awuoropanga <opngabetty98@gmail.com>
2022-09-15 13:22:49 -0700

    50 Fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -5,79 +5,32 @@
  * Return: Nothing.
  */
 
-
 int main(void)
 
-
-
 {
-
 	unsigned long count, i, j, k;
-
-
-
 	i = 0;
-
-
-
 	j = 1;
 
-
-
 	for (count = 0; count < 50; count++)
-
-
-
 	{
-
-
-
 		k = i + j;
-
-
-
 		i = j;
-
-
-
 		j = k;
-
-
 
 		printf("%lu", k);
 
-
-
 		if (count == 49)
 
-
-
 			putchar('\n');
-
-
-
 		else
 
-
-
 		{
-
-
-
 			printf(", ");
-
-
-
 		}
-
-
 
 	}
 
-
-
 	return (0);
-
-
 
 }
