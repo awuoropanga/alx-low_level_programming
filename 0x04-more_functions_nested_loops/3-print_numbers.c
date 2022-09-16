6e394437ec15914f8954cbd1a41d6ada908676e5
awuoropanga <opngabetty98@gmail.com>
2022-09-16 07:43:46 -0700

    zero to nine

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_numbers - func name
+ */
+
+
+void print_numbers(void)
+
+{
+
+		int a;
+		
+
+		for (a = 48; a <= 57; a++)
+
+			{
+
+				_putchar(a);
+							}
+
+					_putchar('\n');
+
+}
