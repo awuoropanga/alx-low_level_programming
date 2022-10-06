4cb26ee2876a8ff1c1c626f98098c572db94177e
awuoropanga <opngabetty98@gmail.com>
2022-10-05 21:48:20 -0700

    name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print file name
+ *
+ * @argc: counts arguments
+ * @argv:  array of arguments
+ *
+ * Return: 0
+ */
+
+
+int main(int argc, char *argv[])
+
+{
+
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+
+}
