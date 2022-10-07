443b18eb8946cdaf25e204ec122314511f9db65e
awuoropanga <opngabetty98@gmail.com>
2022-10-07 08:11:12 -0700

    memory allocation

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,8 +1,5 @@
-_checked(unsigned int b);                                                                                                   
-                                                                                                                                        
-int _putchar(char c);                                                                                                                   
-                                                                                                                                       
-char *string_nconcat(char *s1, char *s2, unsigned int n);                                                                               
-                                                                                                                                        
+void *malloc_checked(unsigned int b);                                                                                  
+int _putchar(char c);                                                                                                              
+char *string_nconcat(char *s1, char *s2, unsigned int n);                                                                                                                    
 void *_calloc(unsigned int nmemb, unsigned int size);                                                                                   
                                                                                 int *array_range(int min, int max);                                                                                                     
